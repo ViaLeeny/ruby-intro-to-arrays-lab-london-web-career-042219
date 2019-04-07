b556cca710183b2ef99bcf9e9c8fa245e054c863
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-07 01:57:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,18 @@
+def instantiate_new_array
+  hello = Array.new()
+  hello
+end 
+
+def array_with_two_elements
+  bye = Array.new()
+  bye << ("hasta luego") 
+  bye << ("nos vemos")
+  bye
+end 
+
+hello = ["mine", "yours", "his"]
+def first_element (hello)
+ hello(0)
+end 
+
+first_element (hello)
